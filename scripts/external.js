3e0fe44e6bd4e4ff685cb11634baebd16f643399
DannyPost <bogdanpostolachi05@gmail.com>
2023-12-01 14:21:35 +0000

    added Tickets page

--- scripts/external.js
+++ scripts/external.js
@@ -1,40 +1,43 @@
 function coinGame(){
+
+	var bTryAgain=true;  //needs attention
 	
-	var bTryAgain=true;
-	
-	while(bTryAgain){
+	while(bTryAgain){    //needs attention
 		
 		//array of coin flip choices
-		var flipChoices=["heads" , "tails"];
+		var flipChoices=["heads" , "tails"];    //works from here 
 		
 		var randomIndex= Math.floor(Math.random()*2);
 		//testing line
 		//alert(flipChoices[randomIndex]);
 		
-		var userF; //variable user's flip
 		var compF; //variable computer's flip
-		var score[0,0]; //zero index= user's score, one index computer's score
+		var score = [0,0]; //zero index= user's score, one index computer's score
 		var message;
 		
 		//user data
-		userF=parseInt(prompt("Select heads or tails"));
+		var userF = prompt("Select heads or tails").toLowerCase(); //.toLowerCase makes all letters lower case 
+		
+		compF = flipChoices;   //needs attention
 		
 		//comp data
-		compF=randomIndex;
+		compF = flipChoices[randomIndex];
 		
-		if (userF==compF){
-			message="You have won";
+		document.getElementById("game").innerHTML = message;
+		
+		if (userF === compF){
+			message = "You have won";
 			score[0]++;
 			
 		}else{
-			message="You have lost";
+			message = "You have lost";
 			score[1]++;
+		}
 	}
 	
-	var sRepeat=prompt("Would you like to play again (Yes or No)?")
-		if (sRepeat="No"||"no");{
+		if (sRepeat .toLowerCase() === "no");{  //needs attention
 			bTryAgain=false;
-			{
+		}
 	
 	
 }//end of function 
